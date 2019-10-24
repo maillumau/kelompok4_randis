8e83311796ebe58a9c016e206f3151f0de44a3b7
onter <libriyantiretno@gmail.com>
2019-10-24 11:35:09 +0700

    Konfigurasi

--- app/src/main/java/com/example/kelasbwh9/randis/Konfigurasi.java
+++ app/src/main/java/com/example/kelasbwh9/randis/Konfigurasi.java
@@ -2,7 +2,7 @@ package com.example.kelasbwh9.randis;
 
 public class Konfigurasi {
 
-    public static final String URL_GET_ALL = "http://kel4.cibuburhijau.buzz/tampil_data.php";
+    public static final String URL_GET_ALL = "http://kel4.cibuburhijau.buzz/tampil_randis.php";
     public static final String URL_GET_BY = "http://kel4.cibuburhijau.buzz/tampil_randis_by_nomor.php?nomor_plat=";
     public static final String URL_HTTP = "http://kel4.cibuburhijau.buzz/";
 
