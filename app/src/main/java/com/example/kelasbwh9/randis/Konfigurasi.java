06facae832009241f747c72cd73672b567f0f263
maillumau <mail.lumau@gmail.com>
2019-10-25 10:00:26 +0700

    perubahan konifgurasi

--- app/src/main/java/com/example/kelasbwh9/randis/Konfigurasi.java
+++ app/src/main/java/com/example/kelasbwh9/randis/Konfigurasi.java
@@ -2,7 +2,7 @@ package com.example.kelasbwh9.randis;
 
 public class Konfigurasi {
 
-    public static final String URL_GET_ALL = "http://randis.cibuburhijau.buzz/tampil_randis.php";
+    public static final String URL_GET_ALL = "http://randis.cibuburhijau.buzz/Api/tampil_randis";
     public static final String URL_GET_BY = "http://randis.cibuburhijau.buzz/Api/tampil_randis_by/";
     public static final String URL_HTTP = "http://randis.cibuburhijau.buzz/";
     public static final String URL_GAMBAR = "http://randis.cibuburhijau.buzz/uploads/foto_mobil/";
